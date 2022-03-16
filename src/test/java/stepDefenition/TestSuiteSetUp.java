c2458fb2be58a4064b009aacda21f6bd61196a2a
shirin8 <100814198+shirin8@users.noreply.github.com>
2022-03-16 19:54:11 -0400

    Webdriver setup in suites steps
    Webdriver chrome added as a default browser using SuiteSetUp with '@Before' and '@After'

---
+++ src/test/java/stepDefenition/TestSuiteSetUp.java
@@ -0,0 +1,28 @@
+package stepDefenition;
+
+public class TestSuiteSetUp {
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+    public class TestSuiteSetUp {
+
+        public static WebDriver chrome;
+
+        @Before
+        public void setupSuite(){
+            WebDriverManager.chromedriver().setup();
+            chrome = new ChromeDriver();
+        }
+
+
+        @After
+        public void lastStep(){
+            chrome.quit();
+        }
+    }
+
+}
