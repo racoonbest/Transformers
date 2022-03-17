e6d4912a89d9f170654152983433b488322f0201
Tony <tulkin4kor@gmail.com>
2022-03-16 20:32:39 -0400

    done
    Co-Authored-By: kseo08 <100814197+kseo08@users.noreply.github.com>

--- src/test/java/stepDefinitions/TestSuiteSetUp.java
+++ src/test/java/stepDefinitions/TestSuiteSetUp.java
@@ -3,10 +3,7 @@ package stepDefinitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
