77d50e0515840590572021d7c08aeba303a8d5fa
Tony <tulkin4kor@gmail.com>
2022-03-18 20:24:21 -0400

    Updated Add review

--- src/test/java/stepDefinitions/AddReview.java
+++ src/test/java/stepDefinitions/AddReview.java
@@ -4,16 +4,19 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.*;
+import pageObjects.Login;
 
 import static stepDefinitions.TestSuiteSetUp.chrome;
 
 
 public class AddReview {
 
+    Login demoShopPage;
     @Given("Customer is on DemoShop home page")
     public void customer_is_on_demo_shop_home_page() {
-        chrome.get("http://demowebshop.tricentis.com/");
-        chrome.manage().window().maximize();
+        demoShopPage = new Login(chrome);
+        demoShopPage.launch();
+
     }
 
     @When("Customer searches for Health Book")
