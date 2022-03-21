d7f66b6f6a7d1a5c607ec4c45bb96c811aae8501
imomali7 <100814145+imomali7@users.noreply.github.com>
2022-03-21 00:40:18 -0400

    Digital Downloads Automation
    happy path successfully passed in automation
    
    Co-Authored-By: Farrukh0770 <100814731+Farrukh0770@users.noreply.github.com>

--- src/test/features/DigitalDownloads.feature
+++ src/test/features/DigitalDownloads.feature
@@ -1,9 +1,7 @@
 Feature:Digital Downloads
   Scenario:  Customer should be able to purchase Digital Downloads
-    Given Customer is on DemoShop home page
-    When Customer clicks on Log in
-    When Customer fills out Log in info
-    When Customer clicks on Log in 2
+    Given Customer is on Log in page
+    When Customer fills out login form
     When Customer clicks on Digital Downloads
     When Customer Adds 3rd Album to Cart
     Then Customer sees verify message
--- src/test/java/stepDefinitions/DigitalDownloads.java
+++ src/test/java/stepDefinitions/DigitalDownloads.java
@@ -30,7 +30,7 @@ public class DigitalDownloads {
        chrome.findElement(By.cssSelector("#payment-method-buttons-container .payment-method-next-step-button")).click();
        chrome.findElement(By.cssSelector("#payment-info-buttons-container .payment-info-next-step-button")).click();
         chrome.findElement(By.cssSelector("#confirm-order-buttons-container .confirm-order-next-step-button")).click();
-        Thread.sleep(3000);
+        Thread.sleep(4000);
     }
 
 }
