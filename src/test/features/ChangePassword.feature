1b998715e7d4c67886d7e6fc63b9ee54e6f30a51
kseo08 <100814197+kseo08@users.noreply.github.com>
2022-03-21 21:02:11 -0400

    update
    1. updated @regression part
    2. given & when for a quick review
    
    Co-Authored-By: imomali7 <100814145+imomali7@users.noreply.github.com>

--- src/test/features/ChangePassword.feature
+++ src/test/features/ChangePassword.feature
@@ -1,4 +1,5 @@
 Feature: Change Password Feature
+  @regression
   Scenario: Verify the customer is able to change their password
-    Given
-    When
+    Given Customer is under login page
+    When Customer tries to login
--- src/test/java/stepDefinitions/ChangePassword.java
+++ src/test/java/stepDefinitions/ChangePassword.java
@@ -1,8 +1,13 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.*;
+
 
 public class ChangePassword {
 
-    @When()
+    @Given("Customer is under login page")
+        public void customer_is_under_login_page(){
+    }
 }
