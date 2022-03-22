fb1f0527384ad3c11851c4d0b3a64216620fee98
kseo08 <100814197+kseo08@users.noreply.github.com>
2022-03-22 16:18:04 -0400

    added homepage & login method
    I accidentally deleted & committed a few minutes ago.

---
+++ src/test/java/pageObjects/HomePage.java
@@ -0,0 +1,16 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    WebDriver driver;
+    String url = "http://demowebshop.tricentis.com/";
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void launch(){
+        driver.get(url);
+    }
+}
