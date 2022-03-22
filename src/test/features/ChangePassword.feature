3bde99c915e79a1dfceaae3583170ed1c7c6dd16
kseo08 <100814197+kseo08@users.noreply.github.com>
2022-03-22 15:58:05 -0400

    update coding _ChangePassword
    1. Feature: modified
    2. Coding is run successfully
    
    Co-Authored-By: imomali7 <100814145+imomali7@users.noreply.github.com>

--- src/test/features/ChangePassword.feature
+++ src/test/features/ChangePassword.feature
@@ -1,9 +1,10 @@
 Feature: Change Password Feature
   @regression
   Scenario: Verify the customer is able to change their password
-    Given Customer is on Log in page
-    When Customer fills out login form
-    When Customer clicks on Email in home page
+    Given Customer is on Login page
+    When Customer enters login credential
+    When Customer clicks on Email on homepage
     When Customer clicks Change password in My account
     When Customer changes password
-    Then Customer sees Password was changed
+    Then Verify Text: Password was changed
+    And Log out and close the window
--- src/test/java/stepDefinitions/ChangePassword.java
+++ src/test/java/stepDefinitions/ChangePassword.java
@@ -1,13 +1,62 @@
 package stepDefinitions;
 
+import pageObjects.Login;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
+import static stepDefinitions.TestSuiteSetUp.chrome;
 
 public class ChangePassword {
 
-    @Given("Customer is under login page")
-        public void customer_is_under_login_page(){
+    Login LogIn;
+
+    @Given("Customer is on Login page")
+    public void customer_is_on_login_page() {
+        LogIn = new Login(chrome);
+        LogIn.launch();
+        chrome.manage().window().maximize();
+    }
+
+    @When("Customer enters login credential")
+    public void customer_enters_login_credential() {
+        LogIn.with("simba10@gmail.com", "simba11");
+    }
+
+    @When("Customer clicks on Email on homepage")
+    public void customer_clicks_on_email_on_homepage() {
+        chrome.findElement(By.cssSelector(".account")).click();
+    }
+
+    @When("Customer clicks Change password in My account")
+    public void customer_clicks_change_password_in_my_account() {
+        chrome.findElement(By.partialLinkText("Change password")).click();
+    }
+
+    @When("Customer changes password")
+    public void customer_changes_password() {
+        chrome.findElement(By.cssSelector("#OldPassword")).sendKeys("simba11");
+        chrome.findElement(By.cssSelector("#NewPassword")).sendKeys("simba1");
+        chrome.findElement(By.cssSelector("#ConfirmNewPassword")).sendKeys("simba1");
+        WebDriverWait wait = new WebDriverWait(chrome, 20);
+        chrome.findElement(By.cssSelector("[value = 'Change password']")).click();
+    }
+
+    @Then("Verify Text: Password was changed")
+    public void verify_text_password_was_changed() {
+        boolean message = chrome.findElement(By.cssSelector(".result")).isDisplayed();
+        System.out.println(message);
+    }
+
+    @And("Log out and close the window")
+    public void log_out_and_close_the_window() {
+        chrome.findElement(By.cssSelector(".ico-logout")).click();
+        chrome.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        chrome.quit();
     }
 }
