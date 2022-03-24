47a7566cf945f0ec19c58502d0aeca454baa0031
kseo08 <100814197+kseo08@users.noreply.github.com>
2022-03-24 19:10:48 -0400

    page factory updated

--- src/test/java/PageObjects/HomePage.java
+++ src/test/java/PageObjects/HomePage.java
@@ -3,6 +3,7 @@ package pageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
     WebDriver driver;
@@ -13,6 +14,7 @@ public class HomePage {
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void launch(){
