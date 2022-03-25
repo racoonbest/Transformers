fa139c43fe0305792a0440392f0cd8d1be4c4659
shirin8 <100814198+shirin8@users.noreply.github.com>
2022-03-25 00:46:01 -0400

    AddReview
    AddReview files have been modified and pom for class created

--- src/test/java/stepDefinitions/AddReview.java
+++ src/test/java/stepDefinitions/AddReview.java
@@ -2,41 +2,36 @@ package stepDefinitions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.*;
+import pageObjects.AddReviewPage;
 import static stepDefinitions.TestSuiteSetUp.driver;
 
 
 public class AddReview {
 
+    AddReviewPage click = new AddReviewPage(driver);
+
     @When("Customer clicks the item")
     public void customer_clicks_the_item() {
-        driver.findElement(By.cssSelector(".product-title")).click();
+        click.productTitle();
     }
 
     @When("Customer clicks Add your review link")
     public void customer_clicks_add_your_review_link() {
-        driver.findElement(By.linkText("Add your review")).click(); // click Add your review link
+        click.addReview();
     }
 
     @Then("Verify text: Only registered users can write reviews")
     public void verify_text_only_registered_users_can_write_reviews() {
-        boolean isDisplayed = driver.findElement(By.cssSelector(".validation-summary-errors")).isDisplayed();
-        System.out.println(isDisplayed);
+       click.errorMsg();
     }
 
     @When("Customer fills out the review form")
-    public void customer_fills_out_the_review_form() throws InterruptedException {
-        driver.findElement(By.cssSelector("#AddProductReview_Title")).sendKeys("I like this book"); // title
-        Thread.sleep(1000);
-        driver.findElement(By.cssSelector("#AddProductReview_ReviewText")).sendKeys("Highly recommend"); // body
-        driver.findElement(By.cssSelector("#addproductrating_5")).click(); // select Excellent for rating
-        Thread.sleep(1000);
-        driver.findElement(By.cssSelector("[value = 'Submit review'")).click();
+    public void customer_fills_out_the_review_form() {
+        click.reviewForm();
     }
 
     @Then("Verify text: Product review is successfully added")
     public void verify_text_product_review_is_successfully_added() {
-        boolean isDisplayed = driver.findElement(By.cssSelector(".result")).isDisplayed();
-        System.out.println(isDisplayed);
+       click.acceptMsg();
     }
 }
